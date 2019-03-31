9cd5f20bb6c51ea2fb6a32d917e0c5c0b287c360
Christopher Spencer <spencerc4@cardiff.ac.uk>
2019-03-31 17:42:27 +0100

    fuc

---
+++ Main.java
@@ -0,0 +1,20 @@
+package rugbyTeam;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String args[]) {
+		// TODO Auto-generated method stub
+		Player player1 = new Player(1, "Team1", "stadium1", "RUF1234b", "John Do", 1);
+		
+		player1.setPlayerID("RUF1234b");
+		System.out.println(player1.getplayerID());
+		UX.printEditMenu();
+		UX.printAddMenu();
+		UX.printEditMenu();
+		UX.printfirstMenu();
+		
+	}
+//this is an edit
+	
+}
