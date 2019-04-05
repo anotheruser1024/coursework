75e4a3277b922812f1ed28f2350e14c484da42bc
Christopher Spencer <spencerc4@cardiff.ac.uk>
2019-04-05 10:10:37 +0100

    all changes to be discared
    old

--- Main.java
+++ Main.java
@@ -5,11 +5,9 @@ public class Main {
 
 	public static void main(String args[]) {
 		// TODO Auto-generated method stub
-		Player player1 = new Player(1, "Team1", "stadium1", "RUF1235", "John Do", 1);
-		Player player2 = new Player(2,"Team1", "stadium1", "RUF12346", "Jane Doe", 10);
-		player2.setCareerTries(1);
-		player1.setCareerTries(10);
-		player1.setPlayerID("RUF12345");
+		Player player1 = new Player(1, "Team1", "stadium1", "RUF1234b", "John Do", 1);
+		
+		player1.setPlayerID("RUF1234b");
 		System.out.println(player1.getplayerID());
 		UX.printEditMenu();
 		UX.printAddMenu();
