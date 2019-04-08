66e4ed91736d9673e929c8c8435ca961e1dea83a
Christopher Spencer <spencerc4@cardiff.ac.uk>
2019-04-08 07:21:20 +0100

    ux updates
    added methods to check user input.

--- Main.java
+++ Main.java
@@ -5,10 +5,11 @@ public class Main {
 
 	public static void main(String args[]) {
 		// TODO Auto-generated method stub
-
-		System.out.println(player1.getplayerID());
-		UX.checkIsInt();
+		Player player1 = new Player("Team1", "stadium1", "RUF1234b", "John Do", 1);
 		
+		player1.setPlayerID("RUF1234b");
+		
+		UX.classPlayerNew();
 	}
 //this is an edit
 	
--- UX.java
+++ UX.java
@@ -42,7 +42,7 @@ public class UX {
 	}
 	public static void printField(String className, String field) {
 		System.out.println("---Enter---");
-		System.out.println("Enter "+ className +"'s "+ field+": ");		
+		System.out.println("Plaese Enter "+ className +"'s "+ field+": ");		
 		
 	}
 	public static  int checkIsInt() {
@@ -57,6 +57,92 @@ public class UX {
 		    number = scanner.nextInt();
 		} while (number <= 0);
 		return number;
+		
+	}
+	public static String inputPlayerID() {
+		String playerID;
+		while(!scanner.hasNext("RUF[0-9][0-9][0-9][0-9][0-9]")){
+			System.out.println("Please enter player ID with Prefix RUF followed by 5 didgits");
+			scanner.next();
+	}
+	playerID = scanner.nextLine();
+	return playerID;
+	}
+	
+	public static String inputName() {
+		//local variables for method 
+		String firstName;
+		String surName;
+		String playerName;
+		
+		System.out.println("please enter Player's First name: ");
+		//checks if input is letters not numbers 
+		while(!scanner.hasNext("[A-Za-z]+")){
+			System.out.print(" Oops!! names can't have numbers");
+			scanner.next();
+			
+		}
+		firstName = scanner.nextLine();
+		//.append(scanner.nextLine());
+		scanner.reset();
+		 
+		System.out.println("Enter player's Surname: ");
+		while(!scanner.hasNext("[A-Za-z]+")){
+			System.out.print("Oops!! Names cant contain numbers");
+			scanner.next();
+		}
+		
+		surName = scanner.nextLine();
+		while(firstName.equals(surName) ) {
+			System.out.println("Oops!! first and last name cant be the same");
+			surName = scanner.next();			
+		}
+		
+		playerName = (firstName +" " + surName);
+		playerName = playerName.toLowerCase();
+		
+		return playerName;
+	}
+	
+	public static String checkIsStr(){
+		String str;
+		//checks if input is letters not numbers 
+		while(!scanner.hasNext("[A-Za-z]+")){
+			System.out.print(" Oops!! names can't have numbers");
+			scanner.next();		
+		}
+		str = scanner.nextLine();
+		//.append(scanner.nextLine());
+		scanner.reset();
+		return str;
+	}
+	
+	
+	public static void classPlayerNew() {
+		String playerID;
+		String stadiumName;
+		String playerName;
+		String teamName;
+		int careerTries;
+		String name;
+		
+		
+		clearScreen();
+		printAddMenu();
+		playerName = inputName();
+		printField("player", "player ID");
+		playerID = inputPlayerID();
+		printField("player", "career Tries");
+		careerTries = checkIsInt();
+		printField("player", "Team name");
+		teamName = checkIsStr();
+		printField("player", "Stadium");
+		stadiumName = checkIsStr();
+		name = playerID;
+		Player name = new Player(teamName, stadiumName, playerID, playerName, careerTries);
+		
+		
+
 		
 	}
 	
